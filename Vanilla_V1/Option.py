c9b6750c01d7429e53549ae06e4bb125868bcf5e
Sacha Luangrath <luangrathsacha5@gmail.com>
2025-12-22 13:23:45 +0100

    Refactor Option class to include value_method and basket_method parameters; add validation for input parameters and enhance repr methods for better readability.

--- Vanilla_V1/Option.py
+++ Vanilla_V1/Option.py
@@ -13,59 +13,123 @@ from Underlying_V1.database import Database
 rounding_precision = 5
 
 class Option:
-    def __init__(self, start_date, end_date, option_type, strike_price, strike_method):
+    def __init__(self, start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method):
 
         if option_type not in ['EU', 'US']:
             raise ValueError("option_type must be either 'EU' or 'US'")
+        
         if start_date >= end_date:
             raise ValueError("start_date must be earlier than end_date") # no intraday options
+        
         if strike_price <= 0:
             raise ValueError("strike_price must be positive") # no null or negative strike price
-        if strike_method not in ['absolute', 'relative']:
-            raise ValueError("strike_method must be either 'absolute' or 'relative'")
+        
+        if value_method not in ['absolute', 'relative']:
+            raise ValueError("value_method must be either 'absolute' or 'relative'")
+        
+        if not isinstance(underlyings, list) or not all(isinstance(u, Underlying) for u in underlyings):
+            raise ValueError("underlyings must be a list of Underlying instances")
+        
+        if basket_method not in ['uniform', 'worst-of', 'best-of']:
+            raise ValueError("basket_method must be either 'uniform', 'worst-of', or 'best-of'")
 
         self.start_date:date = start_date
         self.end_date:date = end_date
         self.maturity_days:int = (end_date - start_date).days
         self.strike_price:float = round(strike_price, rounding_precision)
-        self.strike_method: Literal['absolute', 'relative'] = strike_method
+        self.value_method: Literal['absolute', 'relative'] = value_method
         self.option_type: Literal['EU', 'US'] = option_type
+        self.underlyings:list[Underlying] = underlyings
+        self.basket_method: Literal['uniform', 'worst-of', 'best-of'] = basket_method
 
     def __repr__(self):
         return (f"Option(type={self.option_type}, "
                 f"start_date={self.start_date}, "
                 f"end_date={self.end_date}, "
-                f"strike_price={self.strike_price})")
+                f"strike_price={self.strike_price}), "
+                f"value_method={self.value_method}, "
+                f"basket_method={self.basket_method}, "
+                f"underlyings={self.underlyings}) "
+                f"basket_method={self.basket_method}) ")
     
 class Option_Call(Option):
-    def __init__(self, start_date, end_date, option_type, strike_price, strike_method, underlyings, basket_method, rebate=0.0):
-        super().__init__(start_date, end_date, option_type, strike_price, strike_method)
-        self.basket_method: Literal['uniform', 'worst-of', 'best-of'] = basket_method
-        self.underlyings:list[Underlying] = underlyings
+    def __init__(self, start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method, rebate=0.0):
+        super().__init__(start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method)
         self.rebate: float = rebate
     
     def __repr__(self):
         return (f"Option_Call(type={self.option_type}, "
                 f"start_date={self.start_date}, "
                 f"end_date={self.end_date}, "
-                f"strike_price={self.strike_price}, "
+                f"strike_price={self.strike_price}), "
+                f"value_method={self.value_method}, "
                 f"basket_method={self.basket_method}, "
-                f"underlyings={self.underlyings}), "
+                f"underlyings={self.underlyings}) "
+                f"basket_method={self.basket_method}) ",
                 f"rebate={self.rebate})")
     
 class Option_Put(Option):
-    def __init__(self, start_date, end_date, option_type, strike_price, strike_method, underlyings, basket_method, rebate=0.0):
-        super().__init__(start_date, end_date, option_type, strike_price, strike_method)
-        self.basket_method: Literal['uniform', 'worst-of', 'best-of'] = basket_method
-        self.underlyings:list[Underlying] = underlyings
+    def __init__(self, start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method, rebate=0.0):
+        super().__init__(start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method)
         self.rebate: float = rebate
     
     def __repr__(self):
         return (f"Option_Put(type={self.option_type}, "
                 f"start_date={self.start_date}, "
                 f"end_date={self.end_date}, "
-                f"strike_price={self.strike_price}, "
+                f"strike_price={self.strike_price}), "
+                f"value_method={self.value_method}, "
                 f"basket_method={self.basket_method}, "
-                f"underlyings={self.underlyings}), "
+                f"underlyings={self.underlyings}) "
+                f"basket_method={self.basket_method}) ",
                 f"rebate={self.rebate})")
     
+
+class Digital_Option(Option):
+    def __init__(self, start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method, payout=0.0, rebate=0.0):
+        super().__init__(start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method)
+        self.payout: float = payout
+        self.rebate: float = rebate
+    
+    def __repr__(self):
+        return (f"Digital_Option(type={self.option_type}, "
+                f"start_date={self.start_date}, "
+                f"end_date={self.end_date}, "
+                f"strike_price={self.strike_price}), "
+                f"value_method={self.value_method}, "
+                f"basket_method={self.basket_method}, "
+                f"underlyings={self.underlyings}) "
+                f"basket_method={self.basket_method}) ",
+                f"rebate={self.rebate}) ",
+                f"payout={self.payout})")
+    
+class Digital_Call(Digital_Option):
+    def __init__(self, start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method, payout=0.0, rebate=0.0):
+        super().__init__(start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method, payout, rebate)
+    
+    def __repr__(self):
+        return (f"Digital_Call(type={self.option_type}, "
+                f"start_date={self.start_date}, "
+                f"end_date={self.end_date}, "
+                f"strike_price={self.strike_price}), "
+                f"value_method={self.value_method}, "
+                f"basket_method={self.basket_method}, "
+                f"underlyings={self.underlyings}) "
+                f"basket_method={self.basket_method}) ",
+                f"rebate={self.rebate}) ",
+                f"payout={self.payout})")
+    
+class Digital_Put(Digital_Option):
+    def __init__(self, start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method, payout=0.0, rebate=0.0):
+        super().__init__(start_date, end_date, option_type, strike_price, value_method, underlyings, basket_method, payout, rebate)
+    def __repr__(self):
+        return (f"Digital_Put(type={self.option_type}, "
+                f"start_date={self.start_date}, "
+                f"end_date={self.end_date}, "
+                f"strike_price={self.strike_price}), "
+                f"value_method={self.value_method}, "
+                f"basket_method={self.basket_method}, "
+                f"underlyings={self.underlyings}) "
+                f"basket_method={self.basket_method}) ",
+                f"rebate={self.rebate}) ",
+                f"payout={self.payout})")
