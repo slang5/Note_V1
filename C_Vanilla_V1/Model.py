65aaa3e85cda793957234923e2fedb7c67e40664
Sacha Luangrath <luangrathsacha5@gmail.com>
2025-12-24 02:58:12 +0100

    Implement pricing logic for Digital Options in Vanilla_Model; add price_one_path method and update price method to utilize it

--- C_Vanilla_V1/Model.py
+++ C_Vanilla_V1/Model.py
@@ -2,9 +2,9 @@
 from typing import Union
 from datetime import date
 
-from numpy import typing, float64
+from numpy import typing, float64, where
 
-from C_Vanilla_V1.Option import Option_Call, Option_Put, Digital_Call, Digital_Put
+from C_Vanilla_V1.Option import Digital_Option, Option_Call, Option_Put, Digital_Call, Digital_Put
 from B_Model_V1.base import SimulationConfig
 
 class Vanilla_Model:
@@ -25,3 +25,73 @@ class Vanilla_Model:
         date_indices = [self.config.calendar.get_dates.index(d) for d in self.strikes_dates]
         reduced_paths = self.paths[:, date_indices]
         return reduced_paths
+    
+    @staticmethod
+    def price_one_path(one_path: typing.NDArray[float64], option: Union[Option_Call, Option_Put, Digital_Call, Digital_Put], spot: Union[float, float64]) -> dict[str, Union[float, float64]]:
+        
+        strike = option.strike_price
+        rebate = 0.0
+        payout = 0.0
+
+        price_mean: Union[float, float64] 
+        price_std: Union[float, float64]
+
+        if option.value_method == 'absolute':
+            strike_value = strike
+            rebate = option.rebate
+
+            if isinstance(option, Digital_Option):
+                payout = option.payout
+    
+        elif option.value_method == 'relative':
+            strike_value = strike * spot
+            rebate = option.rebate * spot
+
+            if isinstance(option, Digital_Option):
+                payout = option.payout * spot
+
+        if option.option_type == 'EU':
+            if isinstance(option, Option_Call):
+
+                one_path_final = where(one_path > strike_value, one_path - strike_value, rebate)
+                price_mean = one_path_final.mean()
+                price_std = one_path_final.std()
+                
+            if isinstance(option, Option_Put):
+
+                one_path_final = where(one_path < strike_value, strike_value - one_path, rebate)
+                price_mean = one_path_final.mean()
+                price_std = one_path_final.std()
+
+            if isinstance(option, Digital_Call):
+
+                one_path_final = where(one_path > strike_value, payout, rebate)
+                price_mean = one_path_final.mean()
+                price_std = one_path_final.std()
+            
+            if isinstance(option, Digital_Put):
+
+                one_path_final = where(one_path < strike_value, payout, rebate)
+                price_mean = one_path_final.mean()
+                price_std = one_path_final.std()
+
+        elif option.option_type == 'US':
+            raise NotImplementedError("American option pricing not implemented yet.")
+
+        price = price_mean
+        std = price_std
+        return {"price": price, "std": std}
+
+    def price(self, spot: float = 1.0) -> dict:
+        
+        pricing_dict: dict[date, dict[str, Union[float, float64]]] = {}
+
+        dates_i = 0
+        for path in self.reduce_to_strike_dates().T:
+            price_std = self.price_one_path(path, self.option, spot)
+            pricing_dict[self.strikes_dates[dates_i]] = price_std
+            dates_i += 1
+
+        return pricing_dict
+
+        
